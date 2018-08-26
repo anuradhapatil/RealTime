044e9c8ce193101985c885ec529d657b9706246d
anuradha <patil.anu110@gmail.com>
2018-08-27 02:11:02 +0530

    commit

--- JenkinsSelenium/src/test/java/TestCases/ParallelTestCases.java
+++ JenkinsSelenium/src/test/java/TestCases/ParallelTestCases.java
@@ -1,11 +1,17 @@
 package TestCases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import resources.base;
+
 public class ParallelTestCases {
+	
+	public static Logger log =LogManager.getLogger(ParallelTestCases.class.getName());
 	public WebDriver driver;
 	@Parameters({"browser","url"})
 	@Test
@@ -15,6 +21,7 @@ public class ParallelTestCases {
 			System.out.println(browser);
 			driver.get(url);
 			System.out.println("ParallelTesting in chrome");
+			log.info("In chrome for log");
 	}
 
 }
--- JenkinsSelenium/src/test/java/TestCases/ParallelTestCases2.java
+++ JenkinsSelenium/src/test/java/TestCases/ParallelTestCases2.java
@@ -1,21 +1,28 @@
 package TestCases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+
+
 public class ParallelTestCases2 {
-	public WebDriver driver;
+	
+	public static Logger log =LogManager.getLogger(ParallelTestCases2.class.getName()); WebDriver driver;
 	@Parameters({"browser","url"})
 	@Test
 	public void first(String browser,String url) {
+		
 		 System.setProperty("webdriver.ie.driver","C:\\Users\\shpatil\\Downloads\\IEDriverServer_x64_3.1.0\\IEDriverServer.exe");
 			driver= new InternetExplorerDriver();
 			System.out.println(browser);
 			driver.get(url);
 			System.out.println("ParallelTesting in IE");
+			log.info("In IE for log");
 	}
 
 }
